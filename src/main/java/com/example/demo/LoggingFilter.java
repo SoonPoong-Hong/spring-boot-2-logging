bcad676c9b10241d39591ca8b5669b516913de3f
rocklike <rocklike@gmail.com>
2018-11-18 23:54:30 +0900

    .

--- src/main/java/com/example/demo/LoggingFilter.java
+++ src/main/java/com/example/demo/LoggingFilter.java
@@ -2,17 +2,21 @@ package com.example.demo;
 
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class LoggingFilter implements Filter {
     @Override
     public void doFilter(final ServletRequest req,
                          final ServletResponse res,
                          final FilterChain chain) throws IOException, ServletException {
         final String url = ((HttpServletRequest) req).getRequestURI();
+        log.warn("== {}", url);
 
         if(url.matches("/(health|.+\\.(ico|js))")) {
             req.setAttribute("ignoreLogging", true);
